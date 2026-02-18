065870ed80d7f003cf8bbeefb64a0840552c470b
= <gummy8672@gmail.com>
2026-02-18 18:01:54 +0900

    Fix ts_ls pnpm exec cwd and root detection

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -14,19 +14,13 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-if vim.fn.executable('npm') == 0 then
-  local node_bins = vim.fn.glob(vim.fn.expand('~/.local/share/mise/installs/node/*/bin'), true, true)
-  if #node_bins > 0 then
-    table.sort(node_bins)
-    vim.env.PATH = node_bins[#node_bins] .. ':' .. vim.env.PATH
-  end
-end
-
 vim.opt.number = true
 vim.opt.relativenumber = false
 vim.opt.termguicolors = true
 vim.opt.updatetime = 300
 vim.opt.signcolumn = 'yes'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.laststatus = 3
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'tsx', 'jsx' },
@@ -43,16 +37,25 @@ vim.keymap.set('n', '<M-h>', '<C-w>h', { noremap = true, silent = true })
 vim.keymap.set('n', '<M-j>', '<C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<M-k>', '<C-w>k', { noremap = true, silent = true })
 vim.keymap.set('n', '<M-l>', '<C-w>l', { noremap = true, silent = true })
+vim.keymap.set('n', 'gd', '<cmd>Lspsaga goto_definition<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { noremap = true, silent = true })
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { noremap = true, silent = true })
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, { noremap = true, silent = true })
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ca', '<cmd>Lspsaga code_action<cr>', { noremap = true, silent = true })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { noremap = true, silent = true })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { noremap = true, silent = true })
 
 require('lazy').setup({
   {
     'tpope/vim-sleuth',
-    lazy = false,
   },
   {
     'masisz/wisteria.nvim',
     name = 'wisteria',
-    lazy = false,
     priority = 1000,
     opts = {
       transparent = true,
@@ -65,10 +68,23 @@ require('lazy').setup({
   {
     'neovim/nvim-lspconfig',
     dependencies = {
-      { 'williamboman/mason.nvim', config = true },
+      {
+        'williamboman/mason.nvim',
+        config = function()
+          require('mason').setup({
+            ui = {
+              check_outdated_packages_on_open = false,
+            },
+          })
+        end,
+      },
       { 'williamboman/mason-lspconfig.nvim' },
     },
     config = function()
+      local function ts_root_dir(fname)
+        return vim.fs.root(fname, { 'tsconfig.json', 'package.json', 'jsconfig.json' })
+      end
+
       require('mason-lspconfig').setup({
         ensure_installed = {
           'rust_analyzer',
@@ -96,10 +112,12 @@ require('lazy').setup({
       })
 
       vim.lsp.config('ts_ls', {
-        root_dir = function(fname)
-          return vim.fs.root(fname, { 'tsconfig.json', 'package.json', 'jsconfig.json', '.git' })
-        end,
+        cmd = { 'pnpm', 'exec', 'typescript-language-server', '--stdio' },
+        root_dir = ts_root_dir,
         single_file_support = true,
+        on_new_config = function(new_config, root_dir)
+          new_config.cmd_cwd = root_dir
+        end,
       })
 
       vim.lsp.enable({
@@ -138,8 +156,14 @@ require('lazy').setup({
     'akinsho/toggleterm.nvim',
     version = '*',
     config = function()
-      require('toggleterm').setup({})
-      vim.keymap.set('n', '<leader>tt', '<cmd>ToggleTerm<cr>', { noremap = true, silent = true })
+      require('toggleterm').setup({
+        direction = 'float',
+        close_on_exit = false,
+        shell = 'C:/Users/combl/scoop/shims/nu.exe',
+      })
+      vim.keymap.set('n', '<leader>f', '<cmd>ToggleTerm<cr>', { noremap = true, silent = true })
+      vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+      vim.keymap.set('t', 'jj', [[<C-\><C-n>]], { noremap = true, silent = true })
     end,
   },
   {
@@ -153,6 +177,11 @@ require('lazy').setup({
     config = function()
       require('neo-tree').setup({})
       vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>', { noremap = true, silent = true })
+      vim.api.nvim_create_autocmd('VimEnter', {
+        callback = function()
+          vim.cmd('Neotree show')
+        end,
+      })
     end,
   },
   {
@@ -183,9 +212,93 @@ require('lazy').setup({
     end,
   },
   {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      'lewis6991/gitsigns.nvim',
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      animation = true,
+      auto_hide = false,
+      tabpages = true,
+      clickable = true,
+      focus_on_close = 'left',
+      hide = { extensions = false, inactive = false },
+      highlight_alternate = false,
+      highlight_inactive_file_icons = false,
+      highlight_visible = true,
+      icons = {
+        buffer_index = false,
+        buffer_number = false,
+        button = '',
+        diagnostics = {
+          [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
+          [vim.diagnostic.severity.WARN] = { enabled = true },
+          [vim.diagnostic.severity.INFO] = { enabled = false },
+          [vim.diagnostic.severity.HINT] = { enabled = true },
+        },
+        gitsigns = {
+          added = { enabled = true, icon = '+' },
+          changed = { enabled = true, icon = '~' },
+          deleted = { enabled = true, icon = '-' },
+        },
+        filetype = {
+          custom_colors = false,
+          enabled = true,
+        },
+        separator = { left = '▎', right = '' },
+        separator_at_end = true,
+        modified = { button = '' },
+        pinned = { button = '', filename = true },
+        preset = 'default',
+        alternate = { filetype = { enabled = false } },
+        current = { buffer_index = true },
+        inactive = { button = '' },
+        visible = { modified = { buffer_number = false } },
+      },
+      insert_at_end = false,
+      insert_at_start = false,
+      maximum_padding = 1,
+      minimum_padding = 1,
+      maximum_length = 30,
+      letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+      no_name_title = nil,
+      sort = {
+        ignore_case = true,
+      },
+    },
+    config = function(_, opts)
+      require('barbar').setup(opts)
+      local map = vim.keymap.set
+      local key_opts = { noremap = true, silent = true }
+      map('n', '<Tab>', '<Cmd>BufferNext<CR>', key_opts)
+      map('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', key_opts)
+    end,
+    version = '^1.0.0',
+  },
+  {
     'shellRaining/hlchunk.nvim',
-    lazy = false,
     config = function()
+      local ts = require('hlchunk.utils.ts_node_type')
+      ts.tsx = {
+        'jsx_element',
+        'jsx_fragment',
+        'jsx_self_closing_element',
+        'function_declaration',
+        'arrow_function',
+        'class_declaration',
+        'method_definition',
+        '^if',
+        '^for',
+        '^while',
+        'switch_statement',
+        'try_statement',
+        'catch_clause',
+      }
+      ts.typescript = ts.typescript or ts.tsx
       require('hlchunk').setup({
         indent = {
           enable = true,
@@ -194,7 +307,26 @@ require('lazy').setup({
           },
         },
         chunk = {
-          enable = false,
+          enable = true,
+          priority = 50,
+          notify = true,
+          style = {
+            { fg = "#806d9c" },
+            { fg = "#c21f30" },
+          },
+          use_treesitter = false,
+          chars = {
+            horizontal_line = "─",
+            vertical_line = "│",
+            left_top = "╭",
+            left_bottom = "╰",
+            right_arrow = ">",
+          },
+          textobject = "",
+          max_file_size = 1024 * 1024,
+          error_sign = true,
+          duration = 200,
+          delay = 300,
         },
         line_num = {
           enable = false,
@@ -207,7 +339,6 @@ require('lazy').setup({
   },
   {
     'nvim-treesitter/nvim-treesitter',
-    lazy = false,
     build = ':TSUpdate',
     config = function()
       local ok, configs = pcall(require, 'nvim-treesitter.configs')
@@ -235,6 +366,96 @@ require('lazy').setup({
           additional_vim_regex_highlighting = false,
         },
       })
+    end,
+  },
+  {
+    'numToStr/Comment.nvim',
+    dependencies = {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+    },
+    config = function()
+      require('ts_context_commentstring').setup({
+        enable_autocmd = false,
+      })
+      require('Comment').setup({
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+        toggler = {
+          line = '<leader>cc',
+          block = '<leader>cb',
+        },
+        opleader = {
+          line = '<leader>c',
+          block = '<leader>b',
+        },
+      })
+    end,
+  },
+  {
+    'j-hui/fidget.nvim',
+    opts = {
+      progress = {
+        display = {
+          progress_ttl = 10,
+          done_ttl = 3,
+        },
+      },
+      notification = {
+        override_vim_notify = true,
+      },
+    },
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    opts = {},
+  },
+  {
+    'Wansmer/treesj',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {
+      use_default_keymaps = false,
+      check_syntax_error = true,
+      max_join_length = 120,
+      cursor_behavior = 'hold',
+      notify = true,
+      dot_repeat = true,
+    },
+    config = function(_, opts)
+      require('treesj').setup(opts)
+      local map = vim.keymap.set
+      local key_opts = { noremap = true, silent = true }
+      map('n', '<leader>s', '<Cmd>TSJToggle<CR>', key_opts)
+    end,
+  },
+  {
+    'monaqa/dial.nvim',
+    config = function()
+      local augend = require('dial.augend')
+      require('dial.config').augends:register_group({
+        default = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.date.alias['%Y/%m/%d'],
+          augend.constant.alias.bool,
+        },
+      })
+      local map = vim.keymap.set
+      local key_opts = { noremap = true, silent = true }
+      map('n', '<C-a>', function()
+        require('dial.map').manipulate('increment', 'normal')
+      end, key_opts)
+      map('n', '<C-x>', function()
+        require('dial.map').manipulate('decrement', 'normal')
+      end, key_opts)
+      map('x', '<C-a>', function()
+        require('dial.map').manipulate('increment', 'visual')
+      end, key_opts)
+      map('x', '<C-x>', function()
+        require('dial.map').manipulate('decrement', 'visual')
+      end, key_opts)
     end,
   },
 })
