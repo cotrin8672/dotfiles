064a6ab0732943b08b863582887ecc74175ad0ae
cotrin8672 <gummy8672@gmail.com>
2026-03-04 01:38:28 +0900

    nvim: switch startup benchmarking to vim-startuptime binary and tune snacks

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,38 +1,18 @@
 return {
   'folke/snacks.nvim',
-  enabled = false,
-  keys = {
-    {
-      '<leader>f',
-      function()
-        require('snacks').terminal.toggle('nu')
-      end,
-      desc = 'Toggle Floating Terminal',
-    },
-  },
+  enabled = true,
   opts = {
+    bigfile = {
+      enabled = true,
+    },
     quickfile = {
       enabled = true,
     },
-    terminal = {
-      enabled = true,
-      win = {
-        border = 'rounded',
-        width = 0.75,
-        height = 0.6,
-      },
+    statuscolumn = {
+      enabled = false,
+    },
+    words = {
+      enabled = false,
     },
   },
-  config = function(_, opts)
-    require('snacks').setup(opts)
-
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'snacks_terminal',
-      callback = function(args)
-        local key_opts = { noremap = true, silent = true, buffer = args.buf }
-        vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], key_opts)
-        vim.keymap.set('t', 'jj', [[<C-\><C-n>]], key_opts)
-      end,
-    })
-  end,
 }
