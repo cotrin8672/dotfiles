fed28142b0cd5f0f20297bdf39d517dcff4dd081
cotrin8672 <gummy8672@gmail.com>
2026-03-04 11:44:09 +0900

    nvim: tune blink keymaps to enter+jk workflow

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -4,7 +4,16 @@ return {
   version = '1.*',
   dependencies = { 'rafamadriz/friendly-snippets' },
   opts = {
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'enter',
+      ['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
+      ['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
+      ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
+      ['<C-p>'] = false,
+      ['<C-n>'] = false,
+      ['<Up>'] = false,
+      ['<Down>'] = false,
+    },
     appearance = { nerd_font_variant = 'mono' },
     completion = { documentation = { auto_show = false } },
     sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
