b20dc4523a737b77c7427a7eedda8c6b72495ca3
= <gummy8672@gmail.com>
2026-09-19 13:30:47 +0900

    Improve Snacks picker startup responsiveness

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,5 +1,47 @@
 local float = require("shared.float")
 
+local MD_RENDER_PRELOAD_DELAY = 1500
+local md_render_preview
+
+local function get_md_render_preview()
+	if not md_render_preview then
+		md_render_preview = require("md-render.snacks").preview()
+	end
+	return md_render_preview
+end
+
+local function schedule_md_render_preload()
+	local preload = function()
+		vim.defer_fn(function()
+			pcall(get_md_render_preview)
+		end, MD_RENDER_PRELOAD_DELAY)
+	end
+
+	if vim.v.vim_did_enter == 1 then
+		preload()
+		return
+	end
+
+	local group = vim.api.nvim_create_augroup("SnacksPickerWarmup", { clear = true })
+	vim.api.nvim_create_autocmd("UIEnter", {
+		group = group,
+		once = true,
+		callback = preload,
+	})
+end
+
+local function open_picker(source, opts)
+	opts = vim.tbl_deep_extend("force", {
+		find = false,
+		show_delay = 0,
+	}, opts or {})
+
+	local picker = Snacks.picker(source, opts)
+	picker:show()
+	picker:find()
+	return picker
+end
+
 local function apply_picker_winblend(picker)
 	local wins = {}
 	if picker.layout then
@@ -64,6 +106,7 @@ return {
 	config = function(_, opts)
 		enable_kitty_placeholders_for_wezterm()
 		require("snacks").setup(opts)
+		schedule_md_render_preload()
 	end,
 	opts = {
 		image = {
@@ -83,6 +126,7 @@ return {
 		picker = {
 			enabled = true,
 			ui_select = true,
+			show_delay = 0,
 			config = function(opts)
 				local on_show = opts.on_show
 				opts.on_show = function(picker)
@@ -94,7 +138,7 @@ return {
 				return opts
 			end,
 			preview = function(ctx)
-				return require("md-render.snacks").preview()(ctx)
+				return get_md_render_preview()(ctx)
 			end,
 			win = {
 				input = {
@@ -121,7 +165,7 @@ return {
 		{
 			"<leader>ff",
 			function()
-				Snacks.picker.files()
+				open_picker("files")
 			end,
 			desc = "Find files",
 		},
@@ -132,49 +176,49 @@ return {
 				local sources = require("snacks.picker.config.sources")
 				sources.grep_kensaku = require("plugins.snacks-kensaku.grep_kensaku")
 				sources.grep_merged = require("plugins.snacks-kensaku.grep_merged")
-				Snacks.picker.grep_merged()
+				open_picker("grep_merged")
 			end,
 			desc = "Live grep with grep + kensaku.vim",
 		},
 		{
 			"<leader>fb",
 			function()
-				Snacks.picker.git_branches()
+				open_picker("git_branches")
 			end,
 			desc = "Git branches",
 		},
 		{
 			"<leader>fd",
 			function()
-				Snacks.picker.diagnostics()
+				open_picker("diagnostics")
 			end,
 			desc = "Diagnostics",
 		},
 		{
 			"<leader>fs",
 			function()
-				Snacks.picker.lsp_symbols()
+				open_picker("lsp_symbols")
 			end,
 			desc = "Document symbols",
 		},
 		{
 			"<leader>fS",
 			function()
-				Snacks.picker.lsp_workspace_symbols()
+				open_picker("lsp_workspace_symbols")
 			end,
 			desc = "Workspace symbols",
 		},
 		{
 			"<leader>fi",
 			function()
-				Snacks.picker.gh_issue()
+				open_picker("gh_issue")
 			end,
 			desc = "Github issues",
 		},
 		{
 			"<leader>fp",
 			function()
-				Snacks.picker.gh_pr()
+				open_picker("gh_pr")
 			end,
 			desc = "Github PR",
 		},
