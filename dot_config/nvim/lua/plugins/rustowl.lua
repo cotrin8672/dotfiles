b47146314fe5ad4ccba83378b2d67f65aa3d39ce
= <gummy8672@gmail.com>
2026-09-24 20:46:56 +0900

    Enable RustOwl highlights automatically

--- dot_config/nvim/lua/plugins/rustowl.lua
+++ dot_config/nvim/lua/plugins/rustowl.lua
@@ -3,6 +3,7 @@ return {
 	version = "*",
 	lazy = false,
 	opts = {
+		auto_enable = true,
 		client = {
 			cmd = { "mise", "exec", "--", "rustowl" },
 			cmd_env = { MATLABROOT = vim.env.MATLABROOT or "C:/Program Files/MATLAB/R2024a" },
